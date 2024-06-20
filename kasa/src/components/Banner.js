653da9d7a05296b8d9ed4c22f99a98ee4e04f591
etienne-b01 <e.beuter@wanadoo.fr>
2024-06-21 01:11:17 +0200

    added image files and Prettier configuration files

--- kasa/src/components/Banner.js
+++ kasa/src/components/Banner.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import kasaLogo from '../assets/LOGO_color.svg';
 
 const Banner = () => {
-    return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A propos</Link>
-        </nav>
-    );
+  return (
+    <div>
+      <img src={kasaLogo} />
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/about">A propos</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Banner;
--- kasa/src/components/Home.js
+++ kasa/src/components/Home.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Banner from "./Banner"
-import Card from "./Card"
-
+import React from 'react';
+import Banner from './Banner';
+import Footer from './Footer';
+import Card from './Card';
+import backgroundImage from '../assets/background_image.png';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Accueil</h1>
-            <p>test</p>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <div>
+        <img src={backgroundImage} />
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+      <Card />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
