8ff874d7c929a2cc6c900c2be95b3c8b0b04136e
etienne-b01 <e.beuter@wanadoo.fr>
2024-07-08 10:54:10 +0200

    updated drop-down control arrows

--- kasa/src/components/Collapse/index.js
+++ kasa/src/components/Collapse/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './style.css';
+import CollapseArrow from '../../assets/arrow_collapse.svg';
+import ExpandArrow from '../../assets/arrow_expand.svg';
 
 const Collapse = ({ heading, content }) => {
   const [isActive, setIsActive] = useState(false);
@@ -8,7 +10,12 @@ const Collapse = ({ heading, content }) => {
     <div className="collapse-item">
       <div className="collapse-heading" onClick={() => setIsActive(!isActive)}>
         <div>{heading}</div>
-        <div>{isActive ? '-' : '+'}</div>
+        <div>
+          <img
+            src={isActive ? ExpandArrow : CollapseArrow}
+            className="arrrow_image"
+          />
+        </div>
       </div>
       {isActive && <div className="collapse-content">{content}</div>}
     </div>
