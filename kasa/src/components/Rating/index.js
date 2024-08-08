641a0f0909fe69c02c7ddcb752ae8f5f705fc3a2
etienne-b01 <e.beuter@wanadoo.fr>
2024-08-08 22:17:33 +0200

    Moved the rating mechanism to a dedicated component

---
+++ kasa/src/components/Rating/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import filledRatingStar from '../../assets/rating_star_filled.png';
+import emptyRatingStar from '../../assets/rating_star_empty.png';
+
+function Rating({ rating }) {
+  const starCount = parseInt(rating, 10);
+  const stars = [];
+  for (let i = 0; i < starCount; i++) {
+    stars.push(<img key={i} src={filledRatingStar} alt="rating point" />);
+  }
+  for (let i = 0; i < 5 - starCount; i++) {
+    stars.push(<img key={i} src={emptyRatingStar} alt="empty rating point" />);
+  }
+  return stars;
+}
+
+export default Rating;
--- kasa/src/pages/Gallery/Gallery.jsx
+++ kasa/src/pages/Gallery/Gallery.jsx
@@ -2,34 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import HousingData from '../../data/logements.json';
 import './style.scss';
-import filledRatingStar from '../../assets/rating_star_filled.png';
-import emptyRatingStar from '../../assets/rating_star_empty.png';
+
+import Rating from '../../components/Rating';
+
+const { id } = useParams();
+const housingIdData = HousingData.filter(item => item.id === id);
+const rating = housingIdData[0].rating;
 
 const Gallery = () => {
-  const { id } = useParams();
-  const housingIdData = HousingData.filter(item => item.id === id);
-
-  const generateRatingStars = () => {
-    const starCount = parseInt(housingIdData[0].rating, 10);
-    const stars = [];
-    for (let i = 0; i < starCount; i++) {
-      stars.push(<img key={i} src={filledRatingStar} alt="rating point" />);
-    }
-    for (let i = 0; i < 5 - starCount; i++) {
-      stars.push(
-        <img key={i} src={emptyRatingStar} alt="empty rating point" />
-      );
-    }
-    return stars;
-  };
-
   return (
     <div>
       <h1>{housingIdData[0].title}</h1>
       <img src={housingIdData[0].cover} alt={housingIdData[0].title} />
       <p>{housingIdData[0].description}</p>
       <p>The rating is {housingIdData[0].rating}</p>
-      <div>{generateRatingStars(housingIdData[0].rating)}</div>
+      {<Rating rating={rating} />}
     </div>
   );
 };
