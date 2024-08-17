ffa08fda4efd752d1cf6618af7d976e246b371c3
etienne-b01 <e.beuter@wanadoo.fr>
2024-08-18 01:11:44 +0200

    finalized the collapse mechanism and improved code

--- kasa/src/components/Collapse/index.js
+++ kasa/src/components/Collapse/index.js
@@ -4,16 +4,7 @@ import ExpandArrow from '../../assets/arrow_expand.svg';
 
 const Collapse = ({ heading, content }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-
-  // code for calculating div height
-  useEffect(() => {
-    const contentElement = document.querySelector('.collapse__content');
-    if (isExpanded) {
-      contentElement.style.height = `${contentElement.offsetHeight}px`;
-    } else {
-      contentElement.style.height = '0px';
-    }
-  }, [isExpanded]);
+  const containerRef = useRef();
 
   return (
     <div className="collapse">
@@ -23,21 +14,23 @@ const Collapse = ({ heading, content }) => {
           <div>
             <img
               src={ExpandArrow}
-              onClick={() => {
-                if (isExpanded === false) {
-                  setIsExpanded(true);
-                } else {
-                  setIsExpanded(false);
-                }
-              }}
+              onClick={() => setIsExpanded(!isExpanded)}
               className={
                 isExpanded ? 'arrow-icon rotate-down' : 'arrow-icon rotate-up'
               }
             />
           </div>
         </div>
-        <div className={`collapse__content ${isExpanded ? 'expanded' : ''}`}>
-          {content}
+        <div
+          className="collapse__content--container"
+          ref={containerRef}
+          style={
+            isExpanded
+              ? { height: containerRef.current.scrollHeight + 'px' }
+              : { height: '0px' }
+          }
+        >
+          <div className="collapse__content">{content}</div>
         </div>
       </div>
     </div>
