accb3ffbafe866228064b53e3948df58ce564293
etienne-b01 <e.beuter@wanadoo.fr>
2024-08-29 01:36:27 +0200

    added content to Gallery SCSS and created SCSS for Rating component

--- kasa/src/components/Rating/index.js
+++ kasa/src/components/Rating/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import filledRatingStar from '../../assets/rating_star_filled.png';
 import emptyRatingStar from '../../assets/rating_star_empty.png';
+import './style.css';
 
 function Rating({ rating }) {
   const starCount = parseInt(rating, 10);
   const stars = [];
   for (let i = 0; i < starCount; i++) {
     stars.push(
-      <img key={`filled-${i}`} src={filledRatingStar} alt="rating point" />
+      <img
+        key={`filled-${i}`}
+        src={filledRatingStar}
+        alt="rating point"
+        className="rating-star"
+      />
     );
   }
   for (let i = 0; i < 5 - starCount; i++) {
     stars.push(
-      <img key={`empty-${i}`} src={emptyRatingStar} alt="empty rating point" />
+      <img
+        key={`empty-${i}`}
+        src={emptyRatingStar}
+        alt="empty rating point"
+        className="rating-star"
+      />
     );
   }
   return stars;
